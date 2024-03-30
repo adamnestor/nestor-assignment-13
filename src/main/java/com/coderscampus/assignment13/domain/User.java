fb7b75fa4730efca10e96180f6ed2574e69c38e4
Adam Nestor <79426455+adamnestor@users.noreply.github.com>
2024-03-30 14:53:38 -0400

    updated formatting

--- src/main/java/com/coderscampus/assignment13/domain/User.java
+++ src/main/java/com/coderscampus/assignment13/domain/User.java
@@ -19,73 +19,83 @@ import javax.persistence.Table;
 @Entity // Class name = User, DB Table name = user
 @Table(name = "users")
 public class User {
-	
-	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long userId;
 	private String username;
 	private String password;
 	private String name;
 	private LocalDate createdDate;
-	@ManyToMany(fetch = FetchType.EAGER, 
-			cascade = {CascadeType.PERSIST, CascadeType.MERGE})
-	@JoinTable(name = "user_account",
-			joinColumns = @JoinColumn(name = "user_id"), 
-			inverseJoinColumns = @JoinColumn(name = "account_id"))
+	@ManyToMany(fetch = FetchType.EAGER, cascade = { CascadeType.PERSIST, CascadeType.MERGE })
+	@JoinTable(name = "user_account", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "account_id"))
 	private List<Account> accounts = new ArrayList<>();
-	@OneToOne(mappedBy = "user",
-			cascade = { CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REMOVE }, 
-			orphanRemoval = true)
+	@OneToOne(mappedBy = "user", cascade = { CascadeType.PERSIST, CascadeType.MERGE,
+			CascadeType.REMOVE }, orphanRemoval = true)
 	private Address address;
-	
-	
+
 	public Long getUserId() {
 		return userId;
 	}
+
 	public void setUserId(Long userId) {
 		this.userId = userId;
 	}
+
 	public String getUsername() {
 		return username;
 	}
+
 	public void setUsername(String username) {
 		this.username = username;
 	}
+
 	public String getPassword() {
 		return password;
 	}
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
-	
+
 	public LocalDate getCreatedDate() {
 		return createdDate;
 	}
+
 	public void setCreatedDate(LocalDate createdDate) {
 		this.createdDate = createdDate;
 	}
+
 	public List<Account> getAccounts() {
 		return accounts;
 	}
+
 	public void setAccounts(List<Account> accounts) {
 		this.accounts = accounts;
 	}
+
 	public Address getAddress() {
 		return address;
 	}
+
 	public void setAddress(Address address) {
 		this.address = address;
 	}
+
 	@Override
 	public String toString() {
 		return "User [userId=" + userId + ", username=" + username + ", password=" + password + ", name=" + name
 				+ ", accounts=" + accounts + ", address=" + address + "]";
 	}
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -93,6 +103,7 @@ public class User {
 		result = prime * result + ((userId == null) ? 0 : userId.hashCode());
 		return result;
 	}
+
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
